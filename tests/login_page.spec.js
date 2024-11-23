26d8089e65e3cede145cec1db67ceaff959e76c9
Meeran <meeranmmr@gmail.com>
2024-11-23 10:57:24 +0530

    used tobevisible func

--- tests/login_page.spec.js
+++ tests/login_page.spec.js
@@ -1,6 +1,6 @@
-const { chromium } = require('playwright');
-
-(async () => {
+import { test, expect, chromium } from '@playwright/test';
+import path from 'path';
+test('talentkeepers', async () => {
   const browser = await chromium.launch({
     headless: false,
   });
@@ -8,8 +8,22 @@ const { chromium } = require('playwright');
   const page = await browser.newPage();
 
   await page.goto('https://talentkeepers.com/');
-  await page.waitForNavigation();
+  await page.getByRole('link', { name: 'Get Started Today' }).click();
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('Mohamed');
+  await page.getByPlaceholder('First Name').press('Tab');
+  await page.getByPlaceholder('Last Name').fill('Meeran');
+  await page.getByPlaceholder('Last Name').press('Tab');
+  await page.getByPlaceholder('Company Name').fill('Gyan');
+  await page.getByPlaceholder('Company Name').press('Tab');
+  await page.getByPlaceholder('Job Title').fill('software');
+  await page.getByPlaceholder('Job Title').press('Tab');
+  await page.locator('section').click();
   await page.screenshot({ path: `test.png` });
-
-  await browser.close();
-})();
+  const text = await expect(
+    page.getByText('TalentKeepers has been')
+  ).toBeVisible();
+  console.log(text);
+  await page.goto('https://talentkeepers.com/');
+  await page.close();
+});
