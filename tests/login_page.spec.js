517ed4df8a9b0633d748f39d06deadc850c77bc2
Meeran <151153653+Meeran1998@users.noreply.github.com>
2025-02-19 08:30:20 +0530

    Update login_page.spec.js

--- tests/login_page.spec.js
+++ tests/login_page.spec.js
@@ -2,7 +2,7 @@ import { test, expect, chromium } from '@playwright/test';
 import path from 'path';
 test('talentkeepers', async () => {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
 
   const page = await browser.newPage();
